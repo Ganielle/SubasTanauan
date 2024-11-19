0eabfd7b9a8ff19d0f9295c3a3d346d96178c72a
HeckerCreatives <34593098+Ganielle@users.noreply.github.com>
2024-11-20 07:53:10 +0800

    Update Useridverification.vue

--- src/views/superadmin/manageaccount/Useridverification.vue
+++ src/views/superadmin/manageaccount/Useridverification.vue
@@ -11,7 +11,7 @@
     <!-- Header -->
     <div>
       <div class="mb-3 pt-0" style="display: flex; gap: 10px;">
-        <input type="text" placeholder="Search Customer support" class="px-2 py-1 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline"/>
+        <input type="text" placeholder="Search name" class="px-2 py-1 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline"/>
         
         <button class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
           Clear Search
